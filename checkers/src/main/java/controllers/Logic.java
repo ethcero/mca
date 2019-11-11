489af7cd3c5b07e26ed04f8f5dd2e2babea3b357
Fran <francisco.franco@ethcero.es>
2019-11-11 12:45:12 +0100

    elimina variables locales

--- checkers/src/main/java/controllers/Logic.java
+++ checkers/src/main/java/controllers/Logic.java
@@ -9,20 +9,14 @@ import models.StateValue;
 public class Logic {
 
     private Session session;
-    private StartController startController;
-    private PlayController playController;
-    private ResumeController resumeController;
 	private Map<StateValue, Controller> controllers;
 
 	public Logic() {
         this.session = new Session();
         this.controllers = new HashMap<StateValue, Controller>();
-		this.startController = new StartController(this.session);
-		this.playController = new PlayController(this.session);
-		this.resumeController = new ResumeController(this.session);
-		this.controllers.put(StateValue.INITIAL, this.startController);
-		this.controllers.put(StateValue.IN_GAME, this.playController);
-		this.controllers.put(StateValue.FINAL, this.resumeController);
+		this.controllers.put(StateValue.INITIAL, new StartController(this.session));
+		this.controllers.put(StateValue.IN_GAME, new PlayController(this.session));
+		this.controllers.put(StateValue.FINAL, new ResumeController(this.session));
 		this.controllers.put(StateValue.EXIT, null);
 	}
 
