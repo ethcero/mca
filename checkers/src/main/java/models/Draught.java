58d411c391ee7812e695ef880a5e0d64895013dd
Fran <francisco.franco@ethcero.es>
2019-11-17 19:28:32 +0100

    implementa movimiento largo de dama

--- checkers/src/main/java/models/Draught.java
+++ checkers/src/main/java/models/Draught.java
@@ -12,4 +12,18 @@ class Draught extends Piece {
         assert target != null;
         return Math.abs(origin.getRow() - target.getRow()) > 0;
     }
+
+    @Override
+    Error isCorrect(Coordinate origin, Coordinate target, PieceProvider pieceProvider) {
+        if (!origin.isDiagonal(target)) {
+            return Error.NOT_DIAGONAL;
+        }
+        if (!pieceProvider.isEmpty(target)) {
+            return Error.NOT_EMPTY_TARGET;
+        }
+        if (!this.isAdvanced(origin, target)) {
+            return Error.NOT_ADVANCED;
+        }
+        return null;
+    }
 }
