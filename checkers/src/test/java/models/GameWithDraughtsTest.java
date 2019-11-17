668b2ed7f685726f46130bb2dc7992b9c34fb0f5
Fran <francisco.franco@ethcero.es>
2019-11-17 18:25:51 +0100

    refactor mocked movement

--- checkers/src/test/java/models/GameWithDraughtsTest.java
+++ checkers/src/test/java/models/GameWithDraughtsTest.java
@@ -34,15 +34,9 @@ public class GameWithDraughtsTest {
     public void testGivenGameWhenWhitePawnAtLimitThenNewDraugts(){
         Coordinate origin = new Coordinate(1,0);
         Coordinate target = new Coordinate(0,1);
-        
-        when (turn.getColor()).thenReturn(Color.WHITE);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.WHITE);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.WHITE));
-        
-        when(board.getPiece(target)).thenReturn(new Piece(Color.WHITE));
+
+        this.mockValidMovement(origin, target, Color.WHITE );
+
         game.move(origin, target);
         verify(board).remove(target);
         verify(board).put(any(Coordinate.class), any(Draught.class));
@@ -52,13 +46,9 @@ public class GameWithDraughtsTest {
     public void testGivenGameWhenPawnAtLimitAndEatingThenNewDraugts(){
         Coordinate origin = new Coordinate(2,1);
         Coordinate target = new Coordinate(0,3);
-        when (turn.getColor()).thenReturn(Color.WHITE);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.WHITE);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.WHITE));
-        when(board.getPiece(target)).thenReturn(new Piece(Color.WHITE));
+
+        this.mockValidMovement(origin, target, Color.WHITE );
+
         game.move(origin, target);
         verify(board).remove(origin.betweenDiagonal(target));
         verify(board).remove(target);
@@ -69,15 +59,22 @@ public class GameWithDraughtsTest {
     public void testGivenGameWhenBlackPawnAtLimitThenNewDraugts(){
         Coordinate origin = new Coordinate(6,3);
         Coordinate target = new Coordinate(7,2);
-        when (turn.getColor()).thenReturn(Color.BLACK);
-        when(board.isEmpty(origin)).thenReturn(false);
-        when(board.getColor(origin)).thenReturn(Color.BLACK);
-        when(board.getPiece(origin)).thenReturn(piece);
-        when(piece.isCorrect(origin, target, board)).thenReturn(null);
-        when(board.remove(origin)).thenReturn(new Piece(Color.BLACK));
-        when(board.getPiece(target)).thenReturn(new Piece(Color.BLACK));
+
+        this.mockValidMovement(origin, target, Color.BLACK );
+
         game.move(origin, target);
         verify(board).remove(target);
         verify(board).put(any(Coordinate.class), any(Draught.class));
+    }
+
+    private void mockValidMovement(Coordinate origin, Coordinate target, Color pieceColor){
+        when (turn.getColor()).thenReturn(pieceColor);
+        when(board.isEmpty(origin)).thenReturn(false);
+        when(board.getColor(origin)).thenReturn(pieceColor);
+        when(board.getPiece(origin)).thenReturn(piece);
+        when(piece.isCorrect(origin, target, board)).thenReturn(null);
+        when(board.remove(origin)).thenReturn(new Piece(pieceColor));
+
+        when(board.getPiece(target)).thenReturn(new Piece(pieceColor));
     }
 }
