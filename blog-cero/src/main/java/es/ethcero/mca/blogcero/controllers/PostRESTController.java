f56ea7f07050f60f0a39d937591127411a67a900
Fran <francisco.franco@ethcero.es>
2019-11-25 13:06:13 +0100

    refactor rest controller name

--- blog-cero/src/main/java/es/ethcero/mca/blogcero/controllers/PostRESTController.java
+++ blog-cero/src/main/java/es/ethcero/mca/blogcero/controllers/PostRESTController.java
@@ -1,21 +1,30 @@
 package es.ethcero.mca.blogcero.controllers;
 
 import com.fasterxml.jackson.annotation.JsonView;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import es.ethcero.mca.blogcero.models.Comment;
 import es.ethcero.mca.blogcero.models.Post;
 import es.ethcero.mca.blogcero.responses.ResponseCreatedOrNotFound;
 import es.ethcero.mca.blogcero.responses.ResponseObjectOrNotFound;
 import es.ethcero.mca.blogcero.services.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
-public class PostController {
+public class PostRESTController {
 
     @Autowired
     private PostService service;
