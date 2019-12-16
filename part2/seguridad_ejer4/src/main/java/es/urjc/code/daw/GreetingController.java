6c965f111f33d284a61773c2af4822868ea14fd0
Fran <francisco.franco@ethcero.es>
2019-12-16 17:42:03 +0100

    ejer4 fix replected XSS

--- part2/seguridad_ejer4/src/main/java/es/urjc/code/daw/GreetingController.java
+++ part2/seguridad_ejer4/src/main/java/es/urjc/code/daw/GreetingController.java
@@ -1,5 +1,7 @@
 package es.urjc.code.daw;
 
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,8 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(Model model, @RequestParam String name) {
 
-		model.addAttribute("name", name);
+		PolicyFactory sanitizer = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS);
+		model.addAttribute("name",sanitizer.sanitize( name));
 
 		return "greeting_template";
 	}
