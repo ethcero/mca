bce3404a27d4534d66e5b0f9b9b1d1b382b7f2a4
Fran <francisco.franco@ethcero.es>
2020-02-23 19:54:20 +0100

    cloudformation running

--- blog-cqrs-ejem1/src/main/java/es/codeurjc/daw/repository/CommentRepository.java
+++ blog-cqrs-ejem1/src/main/java/es/codeurjc/daw/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package es.codeurjc.daw.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.codeurjc.daw.model.Comment;
 
+@Repository
 public interface CommentRepository  extends JpaRepository<Comment, Long>{
 
 }
--- blog-cqrs-ejem1/src/main/java/es/codeurjc/daw/repository/PostRepository.java
+++ blog-cqrs-ejem1/src/main/java/es/codeurjc/daw/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package es.codeurjc.daw.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.codeurjc.daw.model.Post;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
 
 }
