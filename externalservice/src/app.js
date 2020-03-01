8d3840157f8b7f79ab2edd839d69514b90cf7053
Fran <francisco.franco@ethcero.es>
2020-03-01 22:43:18 +0100

    bind externalservice to 0.0.0.0

--- externalservice/src/app.js
+++ externalservice/src/app.js
@@ -6,7 +6,7 @@ const server = new grpc.Server();
 
 server.addService(ToUpperCaseService.service, ToUpperCaseImpl);
 
-server.bind('127.0.0.1:8082', grpc.ServerCredentials.createInsecure());
+server.bind('0.0.0.0:8082', grpc.ServerCredentials.createInsecure());
 
 console.log('gRPC server running at http://127.0.0.1:8082');
 
