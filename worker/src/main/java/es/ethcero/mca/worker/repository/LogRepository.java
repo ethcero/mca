93a515637ee10cde7352b936f25817ac529e31c4
Fran <francisco.franco@ethcero.es>
2020-03-21 17:29:30 +0100

    delete code

--- worker/src/main/java/es/ethcero/mca/worker/repository/LogRepository.java
+++
@@ -1,9 +0,0 @@
-package es.ethcero.mca.worker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.ethcero.mca.worker.models.Log;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-
-}
